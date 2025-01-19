fd8360e8d99e96a70b8694f60388f849ce70b521
ManavLohabade <manavlohabade26@gmail.com>
2025-01-20 00:39:15 +0530

    Added All Files

---
+++ src/data/constants.js
@@ -0,0 +1,501 @@
+export const Bio = {
+  name: "Manav Lohabade",
+  roles: [
+    "Full Stack Developer",
+    "Backend Developer",
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm a final-year CSE student with a strong passion for full-stack development, React, and system design. I love exploring new technologies and solving complex problems. My ability to quickly learn and adapt sets me apart, and I’m always eager to build something impactful.",
+  github: "https://github.com/ManavLohabade",
+  resume:
+    "https://drive.google.com/file/d/1S6UMsJ49B5_UbRQ_9D_xP90SToZ1H3LO/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/manav-lohabade/",
+  twitter: "https://twitter.com/BuildWithManav",
+  insta: "https://www.instagram.com/Manavuff",
+  topmate: "https://topmate.io/shiva_mishra/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/768px-Tailwind_CSS_Logo.svg.png?20230715030042",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://static-00.iconduck.com/assets.00/redux-icon-512x486-1n8k02rk.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://static-00.iconduck.com/assets.00/material-ui-icon-512x406-fhnu85xg.png",
+      },
+      {
+        name: "Angular",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+      },
+    ],
+  },
+
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Node Js",
+        image:
+          "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+        "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Django",
+        image:
+          "https://w7.pngwing.com/pngs/885/420/png-transparent-django-hd-logo.png",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://static-00.iconduck.com/assets.00/flask-icon-399x512-v0hqbifs.png",
+      },
+      {
+        name: "Microservices",
+        image:
+        "https://t4.ftcdn.net/jpg/03/85/29/69/360_F_385296927_b9V4skbl94TzxL8AiVAMGdpp9hFeZat0.jpg",
+      },
+    ],
+  },
+
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "C/C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/32/C%2B%2B_logo.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Devops",
+    skills: [
+      {
+        name: "Docker",
+        image: "https://wiki.hornbill.com/images/7/70/Docker_logo.png",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/1200px-Kubernetes_logo_without_workmark.svg.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://www.pngall.com/wp-content/uploads/13/AWS-Logo-PNG-File.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://logos-world.net/wp-content/uploads/2020/09/Linux-Logo.png",
+      },
+      {
+        name: "Jenkins CI/CD",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1200px-Jenkins_logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEkW5QVyzoVT_A8dA6aG4Y6mf_q64EjDpnpRxm49mvUg&s",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://static-00.iconduck.com/assets.00/gitlab-icon-1024x942-f30d1qro.png",
+      },
+
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      // {
+      //   name: "Railway.app",
+      //   image: "https://railway.app/brand/logo-light.png",
+      // },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1166690750/85f69649-5387-44c2-ba45-81ae13812e36-cover.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://i.pinimg.com/736x/74/6d/77/746d77ef9b49afd5ec2306d39592d01e.jpg",
+      },
+      {
+        name: "Abode Photoshop",
+        image:
+          "https://w7.pngwing.com/pngs/301/722/png-transparent-adobe-logo-logos-photoshop-logos-and-brands-icon.png",
+      },
+      // {
+      //   name: "Offset Explorer 2 (Kafka)",
+      //   image:
+      //     "https://static.macupdate.com/products/53532/l/offset-explorer-logo.png?v=1633349541",
+      // },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://ankhub.in/wp-content/uploads/2022/07/Logo-png-2048x1307.png",
+    role: "Jr. Software Engineer Intern",
+    company: "AnkHub Technology Services",
+    date: "Dec 2024 - Present",
+    desc: "Currently pursuing six months Internship",
+    skills: [
+      "React Js",
+      "Angular Js",
+      "Node Js",
+      "JavaScript",
+      "MongoDB",
+      "Web Design",
+      "Bootstrap",
+      "MUI",
+      "Wordpress",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://bizleap.in/wp-content/uploads/2024/08/Logo_1-removebg-preview.png",
+    role: "Digital Marketing Intern",
+    company: "Bizleap - Digital Marketing Agency",
+    date: "Nov 2023 - Jan 2024",
+    desc: "Worked on SEO, SMM, and Email Marketing. I was the top performer in the internship.",
+    skills: [
+      "Digital Advertising",
+      "Email Marketing",
+      "Market Research",
+      "SEO Knowledge",
+      "Meta Business Suite",
+    ],
+  },
+];
+//   {
+//     id: 2,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmMFfnN6owFBveI0QvzutR-6AuTLCvzCgVEo__G25fIA&s",
+//     role: "Java backend Developer",
+//     company: "Suven consultants and private limited",
+//     date: "June 2022 - Dec 2022",
+//     desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the internship.",
+//     skills: [
+//       "Java",
+//       "Spring Framework",
+//       "Microservice",
+//       "Docker",
+//       "Apache Camel",
+//       "Angular Js",
+//       "Junit",
+//     ],
+//   },
+
+//   {
+//     id: 3,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNyqBxDE8sNkJsU3SfmU0woeP3bHGoL37wBuEkRV_dqg&s",
+//     role: "Java Backend Devloper",
+//     company: "AlgorithmXlr8.org",
+//     date: "June 2023 - Mar 2024",
+//     desc: "Built an Coding Platform full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+//     skills: [
+//       "NextJs",
+//       "Redux",
+//       "SadCn",
+//       "Material UI",
+//       "HTML",
+//       "CSS",
+//       "JavaScript",
+//       "Docker",
+//       "AWS",
+//       "MongoDB",
+//       "Spring Framework",
+//       "Spring Microservices",
+//       "kafka",
+//       "Mysql"
+//     ],
+//   },
+
+//   {
+//     id: 4,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSADlmdpwvwazAsMWIgPTKhn7LlgfkmvDr03KvetGuK2Q&s",
+//     role: "Mentor",
+//     company: "Topmate.io",
+//     date: "Aug 2023 - Present",
+//     desc: "Top 1% At this Platfome, Guided over 90+ individuals in DSA, coding, and development, with numbers continuously increasing",
+//     skills: [
+//       "Career Guidance",
+//       "DSA Start From Scratch",
+//       "Development Start From Scratch",
+//       "Linkedin Review",
+//       "Resume Review etc",
+
+//     ],
+
+//   },
+
+// ];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/f/f5/G.H._Raisoni_University_logo.png",
+    school: "G H Raisoni University Amravati",
+    date: "Dec 2022 - May 2025",
+    desc: "I'm currently pursuing my Bachelor's degree in Computer Science Engineering at G H Raisoni University. As part of my coursework, I've taken subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+  },
+
+  {
+    id: 1,
+    img: "https://scontent.fpnq13-1.fna.fbcdn.net/v/t39.30808-6/308456753_468764248599993_6242586617585441677_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=nJe8a5vOVmkQ7kNvgEvU8JU&_nc_zt=23&_nc_ht=scontent.fpnq13-1.fna&_nc_gid=AOVgovjH_yBT8mQMroka8GP&oh=00_AYBE6HS2n-JFuLJ_0hjkeWCTB6fG83jOE0gj0UrU5DlZZA&oe=6789EF64",
+    school: "Sai Polytechnic College Chandrapur",
+    date: "Jul 2019- Mar 2022",
+    desc: "I completed a 3-year Diploma in Computer Engineering at Shri Sai Polytechnic, focusing on programming, web development, networking, and hardware. It provided hands-on experience and sparked my passion for technology.",
+    degree: "Diploma in Computer Engineering",
+  },
+
+  {
+    id: 2,
+    img: "https://png.pngtree.com/png-clipart/20190611/original/pngtree-vector-cartoon-school-png-image_2331288.jpg",
+    school: "Mont Fort Hr. Sec. School, Ballarpur",
+    date: "Apr 2011 - Mar 2019",
+    desc: "I completed my class 10 education at Mont Fort School, where I studied Science with Computer Application.",
+    degree: "CBSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "AlgorithmXlr8",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image: "https://nullsignal.games/wp-content/uploads/2022/10/Nfinity-Account-Siphon.jpg",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "UPI Payment Application",
+    date: "Oct 2022 - Present",
+    description:
+      "UPI payment backend, built on Spring Boot and Microservices, integrates design patterns for streamlined operations and security.",
+    image:
+      "https://static.wikia.nocookie.net/black-plasma-studios/images/a/ad/Null_-_Profile.jpg/revision/latest/scale-to-width-down/350?cb=20170612234434",
+    tags: [
+      "Spring Framework",
+      "Micorservice Arcticture",
+      "Design Patterns",
+      "Apache Kafka",
+      "Apache Camel",
+    ],
+    
+    github: "https://github.com/HimanshuSingour/UPI-Payment-Application",
+  },
+  {
+    id: 2,
+    title: "RailWay Application",
+    date: "Jan 2023 - Mar 2023",
+    description:
+    "Made a railway app like India's own, with all the features you'd expect. Book tickets, check schedules, and travel hassle-free",
+    image:
+      "https://user-images.githubusercontent.com/85048574/164957242-6b476520-dbff-4419-a825-e054e6caf26c.png",
+    tags: [
+      "Spring Framework",
+      "Micorservice Arcticture",
+      "Design Patterns",
+      "Apache Kafka",
+      "Apache Camel",
+    ],
+    github:
+      "https://github.com/HimanshuSingour/India-RailwayWise-Your-Journey-Our-Management",
+  },
+  {
+    id: 3,
+    title: "InfyGo Flight Booking System",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "Built a flight booking app for easy travel planning. Book seats, check flights – all in one place",
+    image:
+      "https://i.ytimg.com/vi/dtTkuy_Jpy8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAYoyggQWDAHKy9vbwvcR2yQOdgOQ",
+    tags: [
+      "Spring Framework",
+      "Micorservice Arcticture",
+      "Design Patterns",
+      "Apache Kafka",
+      "Apache Camel",
+      "Mysql",
+      "MongosDb",
+    ],
+
+    github: "https://github.com/HimanshuSingour/Flight-Booking-Application-",
+  },
+  {
+    id: 10,
+    title: "Movie Mate",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Meet Movi Mate: Your go-to for movies. Find, book, and enjoy your favorite films effortlessly.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1200/1*YikEKg0FVtYuSHSKws7SKg.png",
+    tags: [
+      "Spring Framework",
+      "Micorservice Arcticture",
+      "Design Patterns",
+      "Apache Kafka",
+      "Apache Camel",
+    ],
+
+    github: "https://github.com/HimanshuSingour/MovieMate",
+  },
+
+  {
+    id: 5,
+    title: "Hotel Management",
+    date: "Jun 2021",
+    description:
+      " Meet our Hotel Management System: It handles everything from bookings to checkouts. Easily manage reservations, room assignments, and guest services, making hotel operations smooth as silk.",
+    image: "https://i.ytimg.com/vi/FCCVSvrJLmo/maxresdefault.jpg",
+    tags: [
+      "Spring Framework",
+      "Micorservice Arcticture",
+      "Design Patterns",
+      "Apache Kafka",
+      "Apache Camel",
+    ],
+    github:
+      "https://github.com/HimanshuSingour/Hotel-Management-Microservice-SpringBoot",
+  },
+  {
+    id: 6,
+    title: "Google Pay Application",
+    date: "Jun 2021",
+    description:
+    "A versatile payment application inspired by the original. With features like money transfer, bill splitting, shopping, rewards, and intuitive financial management, our app mirrors the convenience and functionality of Google Pay, ensuring seamless transactions for users",
+    image:
+    "https://i.pcmag.com/imagery/reviews/07y0CcC1S2BwbOvOlSKXTtT-9.fit_lim.size_840x473.v1569478436.png",
+    tags: [
+      "Apache Camel",
+      "Spring Framework",
+      "Micorservice",
+      "Design Patterns",
+      "Apache Kafka",
+    ],
+    github:
+    "https://github.com/HimanshuSingour/Google-Pay-Payment-Application-",
+  },
+  {
+    id: 9,
+    title: "Bank Of Bharat (BOB)",
+    date: "Jun 2023 - Aug 2023",
+    description:
+      "implemented all the functionalities that a typical bank application has in the backend.",
+    image:
+      "https://images.livemint.com/rf/Image-621x414/LiveMint/Period2/2017/10/16/Photos/Incoming/IndusInd-kTRG--621x414@LiveMint.jpg",
+    tags: [
+      "Spring Boot",
+      "Spring MVC",
+      "Hibernate",
+      "Spring Cloud",
+      "Spring Data",
+      "CI/CD",
+      "Apache Camel",
+      "MongoDb",
+      "Msql",
+      "Microservices",
+    ],
+    category: "web app",
+    github: "https://github.com/HimanshuSingour/Bank-Of-Bharat-BOB",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
